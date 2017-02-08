7cf4761925ce05842594b056f1ec0854e5f82a5e
Dustin <dward@unidev.com>
2017-02-08 05:29:03 +0000

    made a templates folder, abstracted html and used more complex code

--- main.py
+++ main.py
@@ -1,124 +1,86 @@
 import webapp2
-import cgi
+import jinja2
+import os
 import re
+from google.appengine.ext import db
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 PASS_RE = re.compile(r"^(?=.*[A-Z])(?=.*[!@#$&%^*()])(?=.*[0-9])(?=.*[a-z]).{7,32}$")
 EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
 
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+								autoescape = True)
+
+def render_str(template, **params):
+	t = jinja_env.get_template(template)
+	return t.render(params)
+
 def valid_username(username):
-	return USER_RE.match(username)
+	return username and USER_RE.match(username)
 
 def valid_password(password):
-	return PASS_RE.match(password)
+	return password and PASS_RE.match(password)
+
+def valid_verification(password, verify):
+	return (password == verify)
 
 def valid_email(email):
-	return EMAIL_RE.match(email)
+	return not email or EMAIL_RE.match(email)
 
-header = """
-	<!DOCTYPE html>
-	<html>
-	    <head>
-	        <style>
-	            .error {
-	                color: red;
-	            }
-	        </style>
-	    </head>
-	    <body>
-	    	<h1>Signup</h1>
-"""
+class BaseHandler(webapp2.RequestHandler):
+	def render(self, template, **kw):
+		self.response.out.write(render_str(template, **kw))
 
-footer = """
-		</body>
-	</html>
-"""
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
 
-full_form ="""
-	        <form method="post">
-	            <table>
-	                <tr>
-	                    <td><label for="username">Username</label></td>
-	                    <td>
-	                        <input name="username" type="text" value="{4}" required>
-	                        <span class="error">{0}</span>
-	                    </td>
-	                </tr>
-	                <tr>
-	                    <td><label for="password">Password</label></td>
-	                    <td>
-	                        <input name="password" type="password" required>
-	                        <span class="error">{1}</span>
-	                    </td>
-	                </tr>
-	                <tr>
-	                    <td><label for="verify">Verify Password</label></td>
-	                    <td>
-	                        <input name="verify" type="password" required>
-	                        <span class="error">{2}</span>
-	                    </td>
-	                </tr>
-	                <tr>
-	                    <td><label for="email">Email (optional)</label></td>
-	                    <td>
-	                        <input name="email" type="email" value="{5}">
-	                        <span class="error">{3}</span>
-	                    </td>
-	                </tr>
-	            </table>
-	            <input type="submit">
-	        </form>
-	        """
-
-class Index(webapp2.RequestHandler):
-
-	def write_form(self, username_error="", password_error="", verification_error="", email_error="", username="", email=""):
-		content = header + full_form.format(username_error, password_error, verification_error, email_error, username, email) + footer
-		self.response.out.write(content)
+class Signup(BaseHandler):
 
 	def get(self):
-		self.write_form()
+		self.render("signup-form.html")
 
 	def post(self):
 		username = self.request.get("username")
 		password = self.request.get("password")
 		verify = self.request.get("verify")
 		email = self.request.get("email")
+		all_valid = True
 
-		username_validate = valid_username(username)
-		password_validate = valid_password(password)
-		verify_validate = password == verify
-		email_validate = valid_email(email)
+		params = dict(username = username,
+						email = email)
 
-		username_error = ""
-		password_error = ""
-		verification_error = ""
-		email_error = ""
+		if not valid_username(username):
+			params["username_error"] = "{0} is not a valid username!".format(username)
+			all_valid = False
 
-		if username_validate and password_validate and verify_validate and ((not email) or (email and email_validate)):
-			self.redirect("/add?username=" + username)
+		if not valid_password(password):
+			params["password_error"] = "A safe password must contain one lowercase, uppercase, special character and number. It also must be between 7 and 32 characters"
+			all_valid = False
 
-		if not username_validate:
-			username_error = "{0} is not a valid username!".format(cgi.escape(username, quote=True))
-
-		if not password_validate:
-			password_error = "A safe password must contain one lowercase, uppercase, special character and number. It also must be between 7 and 32 characters"
-
-		if not verify_validate:
-			verification_error = "Your passwords do not match!"
+		if not valid_verification(password, verify):
+			params["verification_error"] = "Your passwords do not match!"
+			all_valid = False
 		
-		if email and not email_validate:
-			email_error = "{0} is not a valid email!".format(cgi.escape(email, quote=True))
+		if email and not valid_email(email):
+			params["email_error"] = "{0} is not a valid email!".format(email)
+			all_valid = False
 
-		self.write_form(username_error, password_error, verification_error, email_error, username, email)
+		if all_valid:
+			self.render("welcome.html", **params)
+		else:
+			self.render("signup-form.html", **params)
 
-class AddUser(webapp2.RequestHandler):
+class Welcome(BaseHandler):
 
 	def get(self):
 		username = self.request.get("username")
-		self.response.out.write("Welcome, {0}!".format(username))
+		if valid_username(username):
+			self.render("welcome.html",username)
+		else:
+			self.redirect("/")
 
 app = webapp2.WSGIApplication([
-    ('/', Index),
-    ('/add', AddUser)
+    ('/', Signup),
+    ('/welcome', Welcome)
 ], debug=True)
